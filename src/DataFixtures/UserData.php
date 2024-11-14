017c83f742c07017efd74ea0313326e37ce3c75b
blajib <jeanbaptiste.blaschka@gmail.com>
2024-11-14 09:19:46 +0100

    création fixture utilisateur

---
+++ src/DataFixtures/UserData.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\DataFixtures;
+
+class UserData
+{
+    const USERS = [
+        'myriam' => 'myriam@gmail.com',
+        'geoffroy' => 'geoffroy@gmail.com',
+        'laura' => 'laura@gmail.com',
+        'céline' => 'céline@gmail.com',
+        'alice' => 'alice@gmail.com',
+        'laetitia' => 'laetitia@gmail.com'
+    ];
+}
